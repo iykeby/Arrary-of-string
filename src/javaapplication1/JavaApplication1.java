4c7305fdd2aaea35758e2a4add009a47b86966a5
Ikechukwu.Ebele <Ikechukwu.Ebele@ISW-GSDEFT-IE.Interswitchng.com>
2021-09-06 11:41:41 +0100

    First commit

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author Ikechukwu.Ebele
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       // System.out.println("test");
+        // TODO code application logic here
+        String name[] =  {"Tobenna","ik","Ebuka","Yemi","Ebele"};
+        //int x=0;
+//        for(int i =0; i<name.length; i++){
+//            for (x=0; x < name.length;x++){
+//                System.out.println(i+" "+x);
+//               //if (name[i].equals(name[x]) && i!= x) {
+//                  // System.out.println(name[i]);
+//                   
+//               //}
+//            }
+//         
+//        }
+ System.out.print(Arrays.toString(name));
+    }
+    
+}
